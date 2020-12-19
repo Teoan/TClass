06608b20f79fe5ba655f3664ae6e8420d2f880ec
Teoan <d277077411@gmail.com>
2020-12-19 16:12:51 +0800

    update

---
+++ tclassclient/src/utils/auth.js
@@ -0,0 +1,27 @@
+const loginInfoKey = 'loginInfo'
+const EXP = 7 // day
+export function setLoginInfo(data) {
+  localStorageSetItem(loginInfoKey, data)
+}
+
+// EXP * 24 * 60 * 60 * 1 * 1000
+export function getLoginInfo() {
+  return localStorageGetItem(loginInfoKey, EXP * 24 * 60 * 60 * 1 * 1000)
+}
+
+// localStorage的Set方法封装:记录时间
+export function localStorageSetItem(key, value) {
+  const curTime = new Date().getTime()
+  localStorage.setItem(key, JSON.stringify({ data: value, time: curTime }))
+}
+// localStorage的Get方法封装:根据过期时间获取
+export function localStorageGetItem(key, exp) {
+  const data = localStorage.getItem(key)
+  const dataObj = JSON.parse(data)
+  if (new Date().getTime() - dataObj.time > exp) {
+    console.log('用户信息已过期')
+    return {}
+  } else {
+    return dataObj.data
+  }
+}
---
+++ tclassclient/src/views/work/workUploadInfo.vue
@@ -0,0 +1,184 @@
+<template>
+  <span class="workUploadInfo-div">
+    <el-card
+      class="box-card-workuploadinfo"
+      shadow="hover"
+      :body-style="{height:'100%'}"
+    >
+      <span class="work-name">{{ work.name }}</span>
+      <el-divider />
+      <el-tooltip content="选中可进行批量删除" placement="top-start">
+        <el-table
+          v-loading="fileTableLoading"
+          :data="uploadInfo"
+          style="width: 100%"
+          @selection-change="handleSelectionChange"
+        >
+          <el-table-column
+            type="selection"
+            width="55"
+          />
+          <el-table-column
+            prop="sname"
+            label="上传者"
+            width="150"
+          />
+          <el-table-column
+            prop="fileName"
+            label="文件名"
+            width="230"
+          />
+          <el-table-column
+            prop="size"
+            :formatter="fileSizeFormat"
+            label="文件大小"
+            width="100"
+          />
+          <el-table-column
+            prop="createTime"
+            label="上传时间"
+            width="180"
+          />
+          <el-table-column min-width="80" fixed="right">
+            <template slot="header">
+              <el-button v-if="uploadInfo.length>0" size="small" type="primary" @click="downloadFiles">打包下载全部文件</el-button>
+              <el-button v-if="multipleSelection.length > 0" size="small" type="danger" icon="el-icon-delete" @click="deleteFiles">批量删除</el-button>
+            </template>
+            <template slot-scope="scope">
+              <div class="cell-contain">
+                <el-button size="small" type="danger" @click="deleteFile(scope.row)">删除</el-button>
+                <el-button size="small" type="primary" @click="downloadFile(scope.row)">下载</el-button>
+              </div>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-tooltip>
+      <el-pagination
+        background
+        :total="pageInfo.total"
+        :page-count="pageInfo.pages"
+        :current-page="pageInfo.current"
+        hide-on-single-page
+        @current-change="currentChange"
+      />
+    </el-card>
+  </span>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      uploadInfo: null,
+      pageInfo: null,
+      fileTableLoading: false,
+      multipleSelection: []
+    }
+  },
+  computed: {
+    currentUser() {
+      if (this.$store.state.currentUser.role !== undefined) {
+        return this.$store.state.currentUser
+      } else {
+        return JSON.parse(localStorage.getItem('INIT_CURRENTUSER'))
+      }
+    },
+    work() {
+      return JSON.parse(localStorage.getItem('work'))
+    }
+  },
+  created() {
+    this.getUploadInfoByWorkId(1, 12, this.work.id)
+  },
+  methods: {
+    fileSizeFormat(row, column) {
+      return (row.size / 1024 / 1024).toFixed(2) + 'MB'
+    },
+    getUploadInfoByWorkId(current, size, wId) {
+      this.fileTableLoading = true
+      this.getRequest('/admin/work/upload/' + wId, { current: current, size: size }).then(resp => {
+        if (resp.code === 0) {
+          this.pageInfo = resp.data
+          this.uploadInfo = this.pageInfo.records
+          this.fileTableLoading = false
+        }
+      }).catch(error => {
+        this.fileTableLoading = false
+        console.log(error)
+      })
+    },
+    currentChange(current) {
+      this.getUploadInfoByWorkId(current, 12, this.work.id)
+    },
+    deleteFile(upload) {
+      this.fileTableLoading = true
+      this.deleteRequest('/work/deleteFile/', { wId: this.work.id, sId: upload.sid, fileName: upload.fileName }).then(resp => {
+        if (resp.code === 0 && resp.data === true) {
+          this.$message.success('删除成功！')
+          this.fileTableLoading = false
+          this.getUploadInfoByWorkId(this.pageInfo.current, 12, this.work.id)
+        } else {
+          this.fileTableLoading = false
+          this.$message.success(resp.msg)
+        }
+      }).catch(error => {
+        this.fileTableLoading = false
+        this.$message.error(error.msg)
+        console.log(error)
+      })
+    },
+    downloadFiles() {
+      window.open('/admin/work/download/' + this.work.id, '_parent')
+    },
+    downloadFile(upload) {
+      window.open('/admin/work/download/?wId=' + this.work.id + '&fileName=' + upload.fileName, '_parent')
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val
+    },
+    deleteFiles() {
+      this.fileTableLoading = true
+      var deleteFileNameList = []
+      var deleteSIdList = []
+      if (this.multipleSelection.length > 0) {
+        for (var i = 0; i < this.multipleSelection.length; i++) {
+          deleteSIdList[i] = this.multipleSelection[i].sid
+          deleteFileNameList[i] = this.multipleSelection[i].fileName
+        }
+      }
+      this.deleteRequest('/admin/work/upload', { deleteFileNameList: deleteFileNameList, deleteSIdList: deleteSIdList, wId: this.work.id }).then(resp => {
+        if (resp.code === 0 && resp.data === true) {
+          this.$message.success('删除成功！')
+          this.getUploadInfoByWorkId(this.pageInfo.current, 12, this.work.id)
+        }
+        this.fileTableLoading = false
+      }).catch(error => {
+        this.fileTableLoading = false
+        this.$message.error(error.msg)
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.box-card-workuploadinfo {
+    width: 80%;
+    height: 100%;
+}
+.workUploadInfo-div {
+    display: flex;
+    justify-content:center;
+    align-items:center;
+    height: 100%;
+}
+.table-div {
+    margin: 20px 0;
+    height: 80%;
+}
+.work-name {
+  font-size: 25px;
+}
+</style>
