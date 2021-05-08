3275912b25024cdd5c34a0a43107c4ed0766c159
Teoan <d277077411@gmail.com>
2021-05-08 18:06:43 +0800

    注册中心环境搭建

---
+++ tclassserver/tclass-registry/src/main/java/TclassRegistryApplication.java
@@ -0,0 +1,16 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zhuangjy
+ * @description tclass注册中心
+ * @date 2021/5/8 17:34
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class TclassRegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TclassRegistryApplication.class, args);
+    }
+}
